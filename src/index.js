ac2b1054cccd3111aeaffcf09847128cf992918d
Widio Pranoto <widiopranoto@users.noreply.github.com>
2020-12-10 15:32:38 +0700

    10122020

--- src/index.js
+++ src/index.js
@@ -32,4 +32,16 @@ class Car extends React.Component {
   }
 }
 
-ReactDOM.render(<Car />, document.getElementById('root'));
+class Football extends React.Component {
+   shoot = () => { alert(this);}
+    /*
+    The 'this' keyword refers to the component object
+    */
+  render() {
+    return (
+      <button onClick={this.shoot}>Take the shot!</button>
+    );
+  }
+}
+
+ReactDOM.render(<Football/>, document.getElementById('shoot'));
